7c26e2860d135c8adf39f738f034d84320944602
kawa-nobu <44832116+kawa-nobu@users.noreply.github.com>
2023-05-12 17:53:59 +0900

    フィルター機能実装&処理部分変更
    manifestがいろいろ違っていたのでついでに修正した

--- content.js
+++ content.js
@@ -1,12 +1,40 @@
 console.log("Block-Spam-Link-Tweet is Working!");
-const target_elem = document.getElementsByTagName("main")[0];
+
+let block_list;
+let block_regexp;
+fetch(chrome.runtime.getURL('filter.json'), {
+    method: "GET",
+    cache: "no-store"
+}).then(response => {
+    if (!response.ok) {
+        console.error('List load error!');
+    }
+    return response.json();
+}).then(json => {
+    console.log(`List Load!\r\nList update:${json[0].developer_update}\r\nList provider:${json[0].thanks_link}\r\nThanks '${json[0].thanks_name}' !`);
+    block_regexp = new RegExp(json[2].concat_regex);
+    //block_list = json;
+    /*
+    //結合された正規表現を作成するときに使う
+    let concat_list = new Array();
+    for (let index_b = 0; index_b < json[1].length; index_b++) {
+        //console.log(index_b)
+        concat_list.push(`${json[1][index_b].regex}|`)
+    }
+    console.log(`(${concat_list.join("")})`)
+    */
+}).catch(error => {
+    console.error('List load error!', error);
+});
+
+const target_elem = document.getElementById("react-root");
 function run(){
-    let ins_html = `<div style="position: absolute;z-index: 99999;width: 100%;height: 100%;display: flex;align-items: center;text-align: center;justify-content: center;background-color: rgba(0,0,0,0.75);color: #fff;border-radius: 16px;"><p>スパムを検出!<br>クリックでツイートを開く</p></div>`;
     for (let index = 0; index < document.querySelectorAll('[data-testid="card.wrapper"]').length; index++) {
-        if(document.querySelectorAll('[data-testid="card.wrapper"]')[index].innerText.indexOf("bnc.lt") != -1 && document.querySelectorAll('[data-testid="card.wrapper"]')[index].getAttribute("bslt_flag") != "ok"){
+        if(block_regexp.test(document.querySelectorAll('[data-testid="card.wrapper"]')[index].querySelectorAll('[dir="auto"]')[0].innerText) && document.querySelectorAll('[data-testid="card.wrapper"]')[index].getAttribute("cslt_flag") != "ok"){
             //console.log("found!");
             //console.log(document.querySelectorAll('[data-testid="card.wrapper"]')[index]);
-            document.querySelectorAll('[data-testid="card.wrapper"]')[index].setAttribute("bslt_flag", "ok");
+            let ins_html = `<div style="position: absolute;z-index: 99999;width: 100%;height: 100%;display: flex;align-items: center;text-align: center;justify-content: center;background-color: rgba(0,0,0,0.75);color: #fff;border-radius: 16px;"><p>スパムを検出!<br>クリックでツイートを開く</p></div>`;
+            document.querySelectorAll('[data-testid="card.wrapper"]')[index].setAttribute("cslt_flag", "ok");
             document.querySelectorAll('[data-testid="card.wrapper"]')[index].insertAdjacentHTML("beforebegin", ins_html);
         }
     }
@@ -20,3 +48,14 @@ observer.observe(target_elem,{
     attributeOldValue: true,
     characterDataOldValue: true
 });
+/*
+for (let index = 0; index < document.querySelectorAll('[data-testid="card.wrapper"]').length; index++) {
+        if(document.querySelectorAll('[data-testid="card.wrapper"]')[index].querySelectorAll('[dir="auto"]')[0].innerText == 'bnc.lt' && document.querySelectorAll('[data-testid="card.wrapper"]')[index].getAttribute("cslt_flag") != "ok"){
+            //console.log("found!");
+            //console.log(document.querySelectorAll('[data-testid="card.wrapper"]')[index]);
+            let ins_html = `<div style="position: absolute;z-index: 99999;width: 100%;height: 100%;display: flex;align-items: center;text-align: center;justify-content: center;background-color: rgba(0,0,0,0.75);color: #fff;border-radius: 16px;"><p>スパムを検出!<br>クリックでツイートを開く</p></div>`;
+            document.querySelectorAll('[data-testid="card.wrapper"]')[index].setAttribute("cslt_flag", "ok");
+            document.querySelectorAll('[data-testid="card.wrapper"]')[index].insertAdjacentHTML("beforebegin", ins_html);
+        }
+    }
+*/
