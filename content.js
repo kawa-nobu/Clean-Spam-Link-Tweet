f481c66d863423ebd5c254e5d72e0f942475b288
kawa-nobu <44832116+kawa-nobu@users.noreply.github.com>
2026-02-12 01:59:29 +0900

    報告種別のマッピング条件を追加

--- content.js
+++ content.js
@@ -2484,10 +2484,25 @@ async function report_tweet(report_mode, report_element, report_twid, host_mode,
             function send_srv(input_response) {
                 const input_token_convert = decodeURIComponent(input_response.flow_token).replaceAll("=", "");
                 let report_second_stage_body = null;
-                if(!choice_convert_flag && now_steps === 1 &&input_response.subtasks[0].choice_selection?.choices[8]?.id !== "ShownSensitiveDisturbingMediaOption"){
+                if(!choice_convert_flag && now_steps === 1 && input_response.subtasks[0].choice_selection?.choices[8]?.id !== "ShownSensitiveDisturbingMediaOption"){
                     if(user_choice === 8){
+                        //この条件が古くなっている可能性があるが、ページンによって項目が変わってくる仕様のため、念の為残しておく
                         //ステップ1の項目が10種類の場合、「攻撃的な行為や嫌がらせ」にセンシティブが含まれているので切り替える
                         user_choice = 1;
+                        choice_convert_flag = true;
+                    }
+                }
+                if(!choice_convert_flag && now_steps === 1 && input_response.subtasks[0].choice_selection?.choices[8]?.id === "DeceptiveIdentitiesOption"){
+                    if(user_choice === 8){
+                        //ステップ1の項目の8番目が「なりすまし」になっている場合、センシティブの項目が存在しないため「攻撃的な行為や嫌がらせ」に切り替える
+                        user_choice = 1;
+                        choice_convert_flag = true;
+                    }
+                }
+                if(!choice_convert_flag && now_steps === 1 && input_response.subtasks[0].choice_selection?.choices[10]?.id === "CivicIntegrityOption"){
+                    if(user_choice === 10){
+                        //ユーザーページなどでステップ1の項目が11種類の場合、「暴力行為やヘイト行為の主体」が9番目にあるので切り替える
+                        user_choice = 9;
                         choice_convert_flag = true;
                     }
                 }
